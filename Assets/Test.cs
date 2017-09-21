242e83957d2bb0f5ed4788f16ac53d5f8e7a2f76
Henry Jonas Solberg <milkshakeiii@gmail.com>
2017-09-21 14:08:06 -0400

    correct for dopeyness

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,17 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NetMQ;
 
 public class Test : MonoBehaviour
 {
-
 	void Start ()
 	{
-		zmqSocket = new NetMQ.Sockets.PairSocket ();
-		Debug.Log(zmqSocket.Bind ("tcp://*:8889"));
+		NetMQ.Sockets.PairSocket zmqSocket = new NetMQ.Sockets.PairSocket ();
+		zmqSocket.Bind ("tcp://*:8889");
 		string message = "0.1;SESSION;{\"session\":1,\"subject\":\"{{ subject }}\",\"name\":\"{{ experiment }}\", \"version\":\"3.1.0\"}";
 		Debug.Log (message);
 		Debug.Log(zmqSocket.TrySendFrame(message, more: false));
 	}
-
 }
