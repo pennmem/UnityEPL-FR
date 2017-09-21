bbf873f6a9fe4e8fa6647002cd3e1a3051bc1ca4
Henry Jonas Solberg <milkshakeiii@gmail.com>
2017-09-21 14:04:10 -0400

    restore deleted character

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -10,7 +10,7 @@ public class Test : MonoBehaviour
 		zmqSocket = new NetMQ.Sockets.PairSocket ();
 		Debug.Log(zmqSocket.Bind ("tcp://*:8889"));
 		string message = "0.1;SESSION;{\"session\":1,\"subject\":\"{{ subject }}\",\"name\":\"{{ experiment }}\", \"version\":\"3.1.0\"}";
-		Debug.Log(message)
+		Debug.Log (message);
 		Debug.Log(zmqSocket.TrySendFrame(message, more: false));
 	}
 
