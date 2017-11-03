bd767bf05d589b641129c5db15c43b3ec6c2fac0
Henry Jonas Solberg <milkshakeiii@gmail.com>
2017-11-03 17:06:06 -0400

    new sfr

---
+++ Assets/FlyPaths.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct FlyPath
+{
+	[SerializeField] public GameObject[] flyPoints;
+}
+
+public class FlyPaths : MonoBehaviour
+{
+	public GameObject flier;
+	public float speed = 1f;
+	public FlyPath[] flyPaths;
+
+	private bool stopFlight = false;
+
+	void Start()
+	{
+		BeginFlight (0);
+	}
+
+	public void BeginFlight(int pathIndex)
+	{
+		StartCoroutine (DoFlight (pathIndex));
+	}
+
+	public void StopFlight()
+	{
+		stopFlight = true;
+	}
+
+	private IEnumerator DoFlight(int pathIndex)
+	{
+		if (pathIndex < 0 || pathIndex >= flyPaths.Length)
+			throw new UnityException ("That path index doesn't exist.");
+
+		FlyPath flyPath = flyPaths [pathIndex];
+
+		if (flyPath.flyPoints.Length < 2)
+			throw new UnityException ("That path has fewer than two points.");
+
+
+		///////////////Find the distance after each point on the path (not including the last point of course)
+		float[] distancesAfterPoints = new float[flyPath.flyPoints.Length - 1];
+
+		Vector3 originPoint = flyPath.flyPoints [0].transform.position;
+		Vector3 destinationPoint;
+		for (int i = 1; i < flyPath.flyPoints.Length; i++)
+		{
+			destinationPoint = flyPath.flyPoints [i].transform.position;
+			float distanceAfterPoint = Vector3.Distance (originPoint, destinationPoint);
+			distancesAfterPoints [i - 1] = distanceAfterPoint;
+			originPoint = destinationPoint;
+		}
+
+
+		//////////////Fly along at a constant speed
+		Vector3 firstPoint = flyPath.flyPoints[0].transform.position;
+		Vector3 lastPoint = flyPath.flyPoints [flyPath.flyPoints.Length - 1].transform.position;
+		flier.transform.position = firstPoint;
+		flier.transform.LookAt (lastPoint);
+		 
+		int lastPassedPointIndex = 0;
+		float distanceTraveledSinceLastPoint = 0;
+
+		while (!stopFlight)
+		{
+			Vector3 directionToNextPoint;
+			if (lastPassedPointIndex < flyPath.flyPoints.Length - 1)
+				directionToNextPoint = Vector3.Normalize (flyPath.flyPoints [lastPassedPointIndex + 1].transform.position - flyPath.flyPoints [lastPassedPointIndex].transform.position);
+			else
+				directionToNextPoint = flier.transform.forward;
+
+			flier.transform.position = flier.transform.position + directionToNextPoint * speed * Time.deltaTime;
+
+			distanceTraveledSinceLastPoint = Vector3.Distance (flyPath.flyPoints[lastPassedPointIndex].transform.position, flier.transform.position);
+			if (lastPassedPointIndex < flyPath.flyPoints.Length-1 && distanceTraveledSinceLastPoint > distancesAfterPoints [lastPassedPointIndex])
+			{
+				lastPassedPointIndex++;
+				distanceTraveledSinceLastPoint = 0f;
+			}
+
+			yield return null;
+		}
+			
+	}
+}
