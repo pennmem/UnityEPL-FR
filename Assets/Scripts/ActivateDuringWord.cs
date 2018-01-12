ba878f60188b62ad5a99fa05b7e9ff7001deb8b2
Henry Jonas Solberg <milkshakeiii@gmail.com>
2018-01-12 11:14:55 -0500

    position reporting

---
+++ Assets/Scripts/ActivateDuringWord.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDuringWord : MonoBehaviour
+{
+    public GameObject activateMe;
+
+    private void OnEnable()
+    {
+        EditableExperiment.OnStateChange += OnWord;
+    }
+
+    private void OnDisable()
+    {
+        EditableExperiment.OnStateChange -= OnWord;
+    }
+
+    private void OnWord (string stateName, bool on, Dictionary<string, object> extraData)
+    {
+        if (stateName.Equals("WORD") || stateName.Equals("ORIENT"))
+        {
+            activateMe.SetActive(on);
+        }
+    }
+}
---
+++ Assets/Scripts/ReportOnWord.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReportOnWord : MonoBehaviour
+{
+    public WorldDataReporter reporter;
+
+    private void OnEnable()
+    {
+        EditableExperiment.OnStateChange += OnWord;
+    }
+
+    private void OnDisable()
+    {
+        EditableExperiment.OnStateChange -= OnWord;
+    }
+
+    private void OnWord(string stateName, bool on, Dictionary<string, object> extraData)
+    {
+        if (stateName.Equals("WORD") || stateName.Equals("ORIENT"))
+        {
+            reporter.DoReport();
+        }
+    }
+}
