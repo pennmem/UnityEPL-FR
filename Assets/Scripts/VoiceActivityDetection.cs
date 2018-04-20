ccd9cfadf680fc2ce3068abc7d7723022856ba2b
Henry Jonas Solberg <milkshakeiii@gmail.com>
2018-04-20 13:12:29 -0400

    cleanup

---
+++ Assets/Scripts/VoiceActivityDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebRtc;
+
+public class VoiceActivityDetection : MonoBehaviour
+{
+    public RamulatorInterface ramulatorInterface;
+
+    private bool talkingState = false;
+
+	private void Update()
+	{
+        bool someoneIsTalking = SomeoneIsTalking();
+        if (someoneIsTalking != talkingState)
+        {
+            talkingState = someoneIsTalking;
+            ramulatorInterface.SetState("VOCALIZATION", talkingState, new Dictionary<string, object>());
+        }
+	}
+
+    private bool SomeoneIsTalking()
+    {
+        return true;
+    }
+}
