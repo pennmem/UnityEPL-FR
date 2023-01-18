c31f3dc3b25f470a02a61ff89bf7eaab9aabf787
James Bruska <jamesbthecoder@gmail.com>
2023-01-18 16:05:38 -0500

    Added first incomplete version of the new yield-based EventQueue

---
+++ Assets/UnityEPL/Scripts/EventHandling/EventLoop2.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+public class EventLoop2 : EventQueue2 {
+    protected ManualResetEventSlim wait;
+    private CancellationTokenSource tokenSource;
+    private CancellationToken cancellationToken;
+
+    public EventLoop2()
+    {
+        wait = new ManualResetEventSlim();
+        running = false;
+    }
+
+    ~EventLoop2()
+    {
+        StopTimers();
+        wait.Dispose();
+    }
+
+    public void Start(){
+        if(Running()) {
+            return;
+        }
+
+        running = true;
+        Thread loop = new Thread(Loop);
+
+        tokenSource = new CancellationTokenSource();
+        loop.Start(tokenSource.Token);
+    }
+
+    public void Stop(){
+        if(!Running()) {
+            return;
+        }
+
+        running = false;
+        tokenSource.Cancel();
+        tokenSource.Dispose();
+        wait.Set();
+        StopTimers();
+    }
+
+    public void StopTimers() {
+        foreach(var re in repeatingEvents) {
+            re.Stop();
+        }
+        repeatingEvents.Clear();
+    }
+
+    protected void Loop(object token) {
+        cancellationToken = (CancellationToken) token;
+        wait.Reset();
+        while(!cancellationToken.IsCancellationRequested) {
+            bool event_ran = Process();
+            if(!event_ran) {
+                wait.Wait(200);
+                wait.Reset();
+            }
+        }
+    }
+
+    public override void Do(IEvent thisEvent) {
+        base.Do(thisEvent);
+        wait.Set();
+    }
+
+    //public override void DoRepeating(RepeatingEvent thisEvent) {
+    //    // enqueues repeating event at set intervals. If timer isn't
+    //    // stopped, stopping processing thread will still stop execution
+    //    // of events. Timers should only be created if running,
+    //    // otherwise their behavior isn't well defined
+
+    //    if(Running()) {
+    //        base.DoRepeating(thisEvent);
+    //    } else {
+    //        throw new Exception("Can't enqueue an event to a non running Loop");
+    //    }
+    //}
+
+    //// Maybe make all Events in ExperimentBase YieldedFuncs?
+    ////     - Implicit yields
+    //// throw new InvalidOperationException("Cannot call DoYielded if there is already another yielded function in the same EventQueue");
+    //protected class YieldedFunc {
+    //    public bool IsStarted { get; protected set; }
+    //    public bool IsDone { get; protected set; }
+    //    public IEnumerator Enumerator { get; protected set; }
+
+    //    public YieldedFunc(IEnumerator enumerator) {
+    //        this.Enumerator = enumerator;
+    //        this.IsStarted = false;
+    //        this.IsDone = false;
+    //    }
+
+    //    public void Start() {
+    //        this.IsStarted = true;
+    //    }
+
+    //    public bool MoveNext() {
+    //        var result = this.Enumerator.MoveNext();
+    //        this.IsDone = !result;
+    //        return result;
+    //    }
+
+    //    // Current can't be implemented because it would require dynamic keyword
+    //}
+
+    //protected List<YieldedFunc> yieldedFuncs;
+    //public virtual T DoYielded<T>(IEnumerator<T> enumerator) {
+    //    var yieldedFunc = new YieldedFunc(enumerator);
+    //    yieldedFuncs.Add(yieldedFunc);
+    //    DoBlocking(new EventBase(() => {
+    //        // It loops since it is in charge now.
+    //        yieldedFunc.Start();
+    //        LoopYielded(yieldedFunc);
+    //    }));
+        
+    //    return enumerator.Current;
+    //}
+
+    //// TODO: JPB: I may be able to remove the parameter and combine with Loop above
+    ////            Do this by checking if finished is true and it is the lastStarted
+    //// This has the HUGE problem!!!
+    ////     Imagine a situation where you did WaitOnKey and then did WaitOnClassifier
+    ////     If the WaitOnClassifier took 5 seconds, you wouldn't get any key input for those 5 seconds
+    //protected void LoopYielded(YieldedFunc originalYieldedFunc) {
+    //    wait.Reset();
+    //    while (!cancellationToken.IsCancellationRequested) {
+    //        var lastStarted = yieldedFuncs.FindLast(x => x.IsStarted);
+    //        foreach (var yieldedFunc in yieldedFuncs) {
+    //            if (yieldedFunc.IsStarted) {
+    //                var current = yieldedFunc.Enumerator.Current;
+    //                yieldedFunc.MoveNext();
+    //                //if (finished && yieldedFunc == lastStarted) {
+    //                if (yieldedFunc.IsDone && yieldedFunc == originalYieldedFunc) {
+    //                    return;
+    //                }
+    //            }
+    //        }
+
+    //        bool event_ran = Process();
+    //        if (!event_ran) {
+    //            wait.Wait(200);
+    //            wait.Reset();
+    //        }
+    //    }
+    //}
+}
---
+++ Assets/UnityEPL/Scripts/EventHandling/EventQueue2.cs
@@ -0,0 +1,333 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Collections.Concurrent;
+using System;
+using System.Threading.Tasks;
+using System.Collections;
+
+public class YieldedEvent {
+    public bool isStarted { get; protected set; } = false;
+    public bool isFinished { get; protected set; } = false;
+    public IEnumerator enumerator { get; protected set; }
+
+    public YieldedEvent(IEnumerator enumerator, bool isStarted = false) {
+        this.enumerator = enumerator;
+        this.isStarted = isStarted;
+    }
+
+    public bool MoveNext() {
+        this.isStarted = true;
+        this.isFinished = !this.enumerator.MoveNext();
+        return this.isFinished;
+    }
+}
+
+public class YieldedEventQueue {
+    public ConcurrentQueue<Action> eventQueue;
+    protected List<YieldedEvent> yieldedEvents = new List<YieldedEvent>();
+
+    protected volatile int threadID = -1;
+    protected volatile bool running = true;
+
+    public YieldedEventQueue() {
+        this.threadID = Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public bool Process() {
+        // Check all yielded events
+        foreach (var yieldedEvent in yieldedEvents) {
+            yieldedEvent.MoveNext();
+        }
+        yieldedEvents.RemoveAll(x => x.isFinished);
+
+        // Run the new event
+        Action thisEvent;
+        if (running && eventQueue.TryDequeue(out thisEvent)) {
+            try {
+                thisEvent.Invoke();
+            } catch (Exception e) {
+                ErrorNotification.Notify(e);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public virtual void Do(IEnumerator thisEvent) {
+        eventQueue.Enqueue(() => {
+            if (thisEvent.MoveNext()) {
+                // Add event to yieldedEvents list if it didn't finish
+                yieldedEvents.Add(new YieldedEvent(thisEvent, true));
+            }
+        });
+    }
+
+    // TODO: JPB: Add DoRepeating
+    public virtual void DoRepeating(IEnumerator thisEvent, int iterations, int delay, int interval) {
+        throw new NotImplementedException();
+    }
+    public virtual void DoRepeating(RepeatingEvent thisEvent) {
+        throw new NotImplementedException();
+    }
+
+    // Avoid using this if possible
+    // Do not call this from the same thread as the EventQueue/EventLoop
+    public virtual void DoBlocking(IEnumerator thisEvent) {
+        if (this.threadID == Thread.CurrentThread.ManagedThreadId) {
+            throw new InvalidOperationException("Cannot call DoBlocking from the same thread as the EventQueue/EventLoop. It will deadlock.");
+        }
+
+        // Create new task that will block until it finishes
+        var task = new Task(() => {
+            if (thisEvent.MoveNext()) {
+                // Add event to yieldedEvents list if it didn't finish
+                yieldedEvents.Add(new YieldedEvent(thisEvent, true));
+            }
+        });
+        eventQueue.Enqueue(() => task.Start());
+        task.Wait();
+    }
+
+    // Avoid using this if possible
+    // Do not call this from the same thread as the EventQueue/EventLoop
+    public virtual T DoGet<T>(IEnumerator<T> thisEvent) {
+        if (this.threadID == Thread.CurrentThread.ManagedThreadId) {
+            throw new InvalidOperationException("Cannot call DoGet from the same thread as the EventQueue/EventLoop. It will deadlock.");
+        }
+
+        // Create new task that will block until it gets the result
+        var task = new Task<T>(() => {
+            if (thisEvent.MoveNext()) {
+                // Add event to yieldedEvents list if it didn't finish
+                yieldedEvents.Add(new YieldedEvent(thisEvent, true));
+            }
+            return thisEvent.Current;
+        });
+        eventQueue.Enqueue(() => task.Start());
+        return task.Result;
+    }
+}
+
+public class EventQueue2 {
+    public ConcurrentQueue<IEvent> eventQueue;
+
+    protected int repeatingEventID = 0;
+    protected List<RepeatingEvent> repeatingEvents = new List<RepeatingEvent>();
+    protected List<YieldedEvent> yieldedEvents = new List<YieldedEvent>();
+    protected volatile bool running = true;
+
+    public EventQueue2() {
+        eventQueue = new ConcurrentQueue<IEvent>();
+    }
+
+    public virtual void Do(IEvent thisEvent) {
+        eventQueue.Enqueue(thisEvent);
+    }
+
+    //public virtual void DoIn(IEventBase thisEvent, int delay) {
+    //    if(Running()) {
+    //        RepeatingEvent repeatingEvent = new RepeatingEvent(thisEvent, 1, delay, Timeout.Infinite, this);
+    //        DoRepeating(repeatingEvent);
+    //    }
+    //    else {
+    //        throw new Exception("Can't add timed event to non running Queue");
+    //    }
+    //}
+
+    //public virtual void DoRepeating(RepeatingEvent thisEvent) {
+    //    // enqueues repeating event at set intervals. If timer isn't
+    //    // stopped, stopping processing thread will still stop execution
+    //    // of events
+
+    //    if(Running()) {
+    //        // use Do to synchronize repeatingEvents access
+    //        Do(new EventBase<RepeatingEvent>(repeatingEvents.Add, thisEvent));
+    //    }
+    //    else {
+    //        throw new Exception("Can't enqueue repeating event to non running Queue");
+    //    }
+    //}
+
+    //public virtual void DoRepeating(IEventBase thisEvent, int iterations, int delay, int interval) {
+    //    // timers should only be created if running
+    //    DoRepeating(new RepeatingEvent(thisEvent, iterations, delay, interval, this));
+    //}
+
+    // Avoid using this if possible
+    public virtual void DoBlocking(IEventBase thisEvent)
+    {
+        // Create a new EventBase (put in event queue)
+        //    that starts a task that invokes the original event
+        // Then wait on that task to finish
+        var task = new Task(() => thisEvent.Invoke());
+        eventQueue.Enqueue(new BaseEvent(() => task.Start()));
+        task.Wait();
+    }
+
+    // Avoid using this if possible
+    public virtual T DoGet<T>(Task<T> task)
+    {
+        eventQueue.Enqueue(new BaseEvent(() => task.Start()));
+        return task.Result;
+    }
+
+    // Process one event in the queue.
+    // Returns true if an event was available to process.
+    public bool Process() {
+        IEvent thisEvent;
+        if (running && eventQueue.TryDequeue(out thisEvent)) {
+            try {
+                thisEvent.Invoke();
+            } catch(Exception e) {
+                ErrorNotification.Notify(e);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public bool Running() {
+        return running;
+    }
+
+    public void Pause(bool pause) {
+        running = !pause;
+        
+        foreach(RepeatingEvent re in repeatingEvents) {
+            re.Pause(pause);
+        }
+    }
+
+    public void ClearRepeatingEvent(RepeatingEvent thisEvent) {
+        if(repeatingEvents.Contains(thisEvent)) {
+            repeatingEvents.Remove(thisEvent);
+        }
+    }
+}
+
+public interface IEvent {
+    void Invoke();
+}
+
+public class BaseEvent : IEvent {
+    protected Action EventAction;
+
+    public virtual void Invoke() {
+        EventAction?.Invoke();
+    }
+
+    public BaseEvent(Action thisAction) {
+        EventAction = thisAction;
+    }
+}
+
+// Wrapper class to allow different delegate signatures
+// in Event Manager
+public class BaseEvent<T> : BaseEvent {
+    public BaseEvent(Action<T> thisAction, T t) : base(() => thisAction(t)) {}
+}
+
+public class BaseEvent<T, U> : BaseEvent {
+    public BaseEvent(Action<T, U> thisAction, T t, U u) : base(() => thisAction(t, u)) {}
+}
+
+public class BaseEvent<T, U, V> : BaseEvent {
+    public BaseEvent(Action<T, U, V> thisAction, T t, U u, V v) : base(() => thisAction(t, u, v)) {}
+}
+public class BaseEvent<T, U, V, W> : BaseEvent {
+    public BaseEvent(Action<T, U, V, W> thisAction, T t, U u, V v, W w) : base(() =>thisAction(t, u, v, w)) {}
+}
+
+//public class RepeatingEvent : IEventBase {
+
+//    private int iterations;
+//    private readonly int maxIterations;
+//    private int delay;
+//    private int interval;
+//    public readonly ManualResetEventSlim flag;
+//    private Timer timer;
+//    private DateTime startTime;
+//    private IEventBase thisEvent;
+//    private EventQueue queue;
+
+//    public RepeatingEvent(IEventBase originalEvent, int _iterations, int _delay, int _interval,
+//                          EventQueue _queue, ManualResetEventSlim _flag = null) {
+//        maxIterations = _iterations;
+//        delay = _delay;
+//        interval = _interval;
+//        startTime = HighResolutionDateTime.UtcNow;
+//        queue = _queue;
+
+
+//        if (_flag == null) {
+//            flag = new ManualResetEventSlim();
+//        } else {
+//            flag = _flag;
+//        }
+
+//        thisEvent = originalEvent;
+//        SetTimer();
+//    }
+
+//    public RepeatingEvent(Action _action, int _iterations, int _delay,
+//                          int _interval, EventQueue _queue,
+//                          ManualResetEventSlim _flag = null)
+//                          : this(new EventBase(_action),
+//                                 _iterations, _delay,
+//                                 _interval, _queue, _flag) {
+//    }
+
+//    private void SetTimer() {
+//        TimerCallback callback = (Object obj) => {
+//            // event is a keyword
+//            var evnt = (RepeatingEvent)obj;
+//            if (!evnt.flag.IsSet) {
+//                queue.Do(evnt);
+//            }
+//        };
+
+//        this.timer = new Timer(callback, this, delay, interval);
+//    }
+
+//    public void Invoke() {
+//        if (!(maxIterations < 0) && (iterations >= maxIterations)) {
+//            Stop();
+//            return;
+//        }
+
+//        Interlocked.Increment(ref this.iterations);
+//        thisEvent.Invoke();
+//    }
+
+//    public void Pause(bool pause) {
+//        DateTime time = HighResolutionDateTime.UtcNow;
+//        // examples don't check success of Change
+//        if (pause) {
+//            flag.Set();
+//            delay -= (int)((TimeSpan)(time - startTime)).TotalMilliseconds;
+//            if (delay <= 0) {
+//                // Set delay to be the remaining portion of interval
+//                // |----||----------||-----------|
+//                // delay    interval    interval
+//                //         elapsed          |
+//                // remaining = interval - (elapsed - delay) % interval
+//                // C# mod is remainder, rather than mod,
+//                // so it's happy with negative values here
+//                delay = interval + (delay % interval);
+//            }
+//            timer?.Change(Timeout.Infinite, Timeout.Infinite);
+//        } else {
+//            flag.Reset();
+//            startTime = time;
+//            timer?.Change(delay, interval);
+//        }
+//    }
+
+//    public void Stop() {
+//        flag.Set();
+//        timer?.Dispose();
+//        // set timer to null in case Pause is queued before event is removed
+//        timer = null;
+//        queue.Do(new EventBase<RepeatingEvent>(queue.ClearRepeatingEvent, this));
+//    }
+//}
