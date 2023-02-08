babb40bb9b1f8256fdd88fed40e52e0d2e423993
James Bruska <jamesbthecoder@gmail.com>
2023-02-08 13:47:28 -0500

    Upgraded repo from 2019.4.0f1 to 2022.2.3f1

---
+++ Assets/Scripts/Experiments/ApemRepFRExperiment.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApemRepFRExperiment : RepFRExperiment {
+    public ApemRepFRExperiment(InterfaceManager _manager) : base(_manager) {}
+
+
+}
