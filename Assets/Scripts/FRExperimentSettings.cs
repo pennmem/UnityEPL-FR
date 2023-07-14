5fb5327dac7bbdaca05034879ad96be529d8450f
psybruce <brucec8@mcmaster.ca>
2023-07-14 14:54:00 -0400

    Updated task version number to 1.6.9

--- Assets/Scripts/FRExperimentSettings.cs
+++ Assets/Scripts/FRExperimentSettings.cs
@@ -13,7 +13,7 @@ public enum DistractionType {
 /// </summary>
 public struct ExperimentSettings
 {
-    public const string taskVersion = "1.6.7";
+    public const string taskVersion = "1.6.9";
 
     public WordListGenerator wordListGenerator; //how the words for this experiment will be created and organized.  for a full list of parameters and what they do, see the comments for WordListGenerator in WordListGenerator.cs
     public string experimentName; //the name of the experiment.  this will be displayed to the user and sent to ramulator.
